121b231756a9cdd51dfa173db4113ba8763e224d
iamsamelea <iamsamelea.code@gmail.com>
2022-05-01 17:59:18 +0200

    encriptacion hecha al 90%

--- gestinf_equip4_m8m9m8/gestinfEquip4/login.php
+++ gestinf_equip4_m8m9m8/gestinfEquip4/login.php
@@ -16,17 +16,9 @@ if (isset($_POST["correu-entra"]) && isset($_POST["pass-entra"])) { //comprova s
   $_SESSION['correu_sessio'] = $correuEntra; //es guarda a la variable global $_SESSION el correu de l'usuari, entre els claudators fiquem el nom de la variable
 
   
-
   
-  
-  //$db_host="mariadb"; //contenedor, nom de la maquina o IP
-  //$port="3306";
-  //$db="gestinf"; //nom base de dades
-  //$user="root"; //root //usuari
-  //$pass="rootpwd";//"clauusuari" //contrasenya
 
-  //variable que cridarem en el query,per tal de verificar el nom d'usuari i contrassenya
-  //$sql = "SELECT Usuaris.correu,Usuaris.contrasenya,Tipus.tipus FROM Usuaris INNER JOIN Tipus ON Usuaris.Tipus_id =".$tipus." WHERE Usuaris.correu = '" .$mailEntra."'";
+
   $sql = "SELECT Correu_usuari, Contrasenya_usuari, Tipus_usuari FROM Usuaris WHERE Correu_usuari='$correuEntra' AND Contrasenya_usuari='$passEntra'";
           //SELECT d'exemple, ajustar-lo als parametres de la nostra base de dades.
           //com a exemple tenim una variable tipus
@@ -36,23 +28,35 @@ if (isset($_POST["correu-entra"]) && isset($_POST["pass-entra"])) { //comprova s
   //per fer-ho amb hard code podem possar-ho entre cometes ex: $db_host = "mariadb"
 
 
+
+
   $result = mysqli_query($conn, $sql); //la funcio query executa la consulta i ens tornara el resultat d'aquesta. Amb query podem fer, selects, inserts,i tal.
   //entre parentesis seguit del query introduim la consulta, en aquesta ocasió hi ha una variable que contindra la consulta
 
-  $conn -> close(); //tanquem la connexió a la base de dades per a evitar problemes de seguretat, en aquest punt la consulta o query ja está assignada com a valor a la variable $result que és amb la que treballarem
+  
+  //echo $contrasenyaEncriptada;
+  
 
 
   if ($result -> num_rows > 0){ //si resultat torna més de 0 línies entrará al if, si no va al else
       //echo $result;
 
-      $row = $result->fetch_assoc();
-
+      $row = $result -> fetch_assoc(); //guardem a la variable $row un array de la variable $result
+      $correu_db = $row['Correu_usuari'];
+      $contra_db = $row["Contrasenya_usuari"];
+      //utilitzem la funció password_hash i li passarem dos parametres, la variable de la contrasenya sense encriptar i PASSWORD_DEFAULT que la seva funció serà la de generar la contrasenya encriptada
+      $contrasenyaEncriptada = password_hash($passEntra, PASSWORD_BCRYPT); //utilitzem la funció password_hash per a encriptar la contrasenya
+      //echo $contrasenyaEncriptada;
+      
       //print_r($row);
 
-      $contra_db = $row["Contrasenya_usuari"];
+      
+      
 
 
-      if($passEntra == $contra_db){ //si contrasenya es igual a contrasenya de la base de dades
+      
+      //if($nom . password_verify($contrasenya, $contra_db) == $nom_db . password_verify($contra_db, $contrasenya)){
+      if(password_verify($passEntra, $contrasenyaEncriptada) && ($correuEntra == $correu_db)){ //desencripta la contrasenya 
         if($row["Tipus_usuari"]=="Alumne"){ //si el tipus es Alumne ens dirigira a el dashboard alumnes
 
 
@@ -86,7 +90,7 @@ if (isset($_POST["correu-entra"]) && isset($_POST["pass-entra"])) { //comprova s
     die();
   }
 
-  $conn = close(); //mata la connexió
+  $conn -> close(); //tanquem la connexió a la base de dades per a evitar problemes de seguretat, en aquest punt la consulta o query ja está assignada com a valor a la variable $result que és amb la que treballarem
 
 } else{
   //redirecció a l'index perque no s'han ficat els camps necessaris
